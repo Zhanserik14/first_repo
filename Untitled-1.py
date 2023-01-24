17bc0278d3f2c8d22d33657cdb7cb4a93ac3218a
Zhanserik14 <122849168+Zhanserik14@users.noreply.github.com>
2023-01-24 10:07:07 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+name = "Zhangir"
+surname = "Rakhymgaliyev"
+patronymic = "Zhanbolatovich"
+
+profession ="footballer"
+
+print( "Full name: %s %s %s"  %(name, surname, patronymic))
+print("profession: %s" % (profession))
+
+#vvod
+print("are you footballer?")
+vvod = str(input())
+#uslovnye operatory
+if vvod == "yes":
+    print(True)
+
+elif vvod == "no, i'm doctor":
+    print(" you're lying")
+else :
+    print(False)
+ 
